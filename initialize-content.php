fee991df39eec4f77bccfdfdd90cb7c56f9c1f7e
Konstantinos Kokkorogiannis <koyandianthos@gmail.com>
2018-03-30 14:29:22 +0300

    Testing different slug for press-release to debug continous creation

--- initialize-content.php
+++ initialize-content.php
@@ -157,7 +157,7 @@ $p4_page_type_terms = [
 	],
 	'1' => [
 		'name'        => 'Press release',
-		'slug'        => 'press-release',
+		'slug'        => 'press-release-cd',
 		'description' => 'A term for press release post type',
 	],
 	'2' => [
